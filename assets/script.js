8e6541722a11b6a7e37254f1be0f899adf3b64f3
sandraols <sandra.helene.olsson@gmail.com>
2020-04-24 17:42:35 +0200

    add display consultants by name

--- assets/script.js
+++ assets/script.js
@@ -11,9 +11,30 @@ const source = document.getElementById('consultant-template').innerHTML;
 const consultantBoxTemplate = Handlebars.compile(source);
 
 $.get('https://api.tretton37.com/ninjas', function(consultants) {
-    console.log(consultants);
-     const display = consultantBoxTemplate(consultants);
-     console.log(display);
+    //display consultants by name
+    const consultantsSortedByName = consultants.sort(function(a, b) {
+        var nameA = a.name.toUpperCase();
+        var nameB = b.name.toUpperCase(); 
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      });
+    //sort consultants by office
+    // const consultantsSortedByOffice = consultants.sort(function(a, b) {
+    // var nameA = a.office.toUpperCase();
+    // var nameB = b.office.toUpperCase(); 
+    // if (nameA < nameB) {
+    //     return -1;
+    // }
+    // if (nameA > nameB) {
+    //     return 1;
+    // }
+    // return 0;
+    // });
+    const display = consultantBoxTemplate(consultants);
     $( ".consultants__container" ).append(display);
-    
 });
